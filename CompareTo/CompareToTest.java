4219a9bcc603ae68d8ce22860d25498447b1b818
SarahNurhasna <114993457+SarahNurhasna@users.noreply.github.com>
2022-11-15 18:53:54 +0700

    Add files via upload

---
+++ CompareTo/CompareToTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CompareTo;
+
+/**
+ *
+ * @author Asus
+ */
+public class CompareToTest {
+    public static void main(String[] args) {
+        String name = "sarah";
+        String name1 = "Sarah";
+
+        if(name == name1)
+            System.out.println("sarah is equal to Sarah");
+        else
+            System.out.println("sarah is not equal to Sarah");
+    }
+}
